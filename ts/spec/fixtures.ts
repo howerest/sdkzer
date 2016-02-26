c973e99f7f2a8080e64c31e595505f209f11d08e
David Valin <davidvalin@howerest.com>
2016-02-26 22:52:34 +0100

    More test cases. prevValue passing. Moving mock test data to fixtures.ts

---
+++ ts/spec/fixtures.ts
@@ -0,0 +1,46 @@
+/**
+    This sdk entity is being used for unit testing to test communication
+    between local entity data and origin data-source
+ */
+
+var buildSdkzerModelEntity = () => {
+  class Item extends Sdkzer {
+    public resourceEndpoint() {
+      return 'http://api.mydomain.com/v1/items';
+    }
+
+    public defaults() {
+      return {
+        name: "A good choice",
+        items: [1, 100, 1, 60]
+      };
+    }
+
+    public parseOne(data) {
+      // Parse record
+      var json = {
+        id: data.id,
+        name: data.name,
+        items: data.items
+      };
+
+      return json;
+    }
+
+    public parse(data) {
+      var parsed = [];
+
+      if (Array.isArray(data)) {
+        for (var i = 0; i < data.length; i++) {
+          parsed.push(this.parseOne(data[i]));
+        }
+      } else {
+        parsed.push(this.parseOne(data.folder));
+      }
+
+      return parsed;
+    }
+  }
+
+  return Item;
+}
