d53fdf1f0cbaffb6e54d8bda5049d7d28873bac6
David Valin <davidvalin@howerest.com>
2017-01-10 16:54:05 +0100

    0.1.0-rc.3

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,48 +3,9 @@ module.exports = function(grunt) {
   grunt.initConfig({
     watch: {
       files: [
-        './js/**/*.js'
+        './built/**/*.js'
       ],
       tasks: ['default']
-    },
-
-    //  Code concatenation
-    // ------------------------------------------------------------------------
-    concat: {
-      options: {
-        // define a string to put between each file in the concatenated output
-        separator: ';',
-        uglify: false
-      },
-      release: {
-        // the files to concatenate
-        src: [
-          // Uncomment next line for ES3 support if you don't include any ES6 Promise polifil in a parent build
-          //'node_modules/es6-promise/dist/es6-promise.js',
-          'node_modules/js-webservices/js/util.js',
-          'node_modules/js-webservices/js/web_services.js',
-          'js/ts/howerest.modularizer.js',
-          'js/ts/howerest.sdkzer.js'
-        ],
-        // the location of the resulting JS file
-        dest: 'release/howerest-sdkzer.js'
-      }
-    },
-
-    //  Code uglification
-    // ------------------------------------------------------------------------
-    uglify: {
-      options: {
-        compress: {
-          drop_console: true
-        },
-        banner: "/*! howerest-sdkzer 0.1.0-rc.2 | howerest 2016 - <davidvalin@howerest.com> | Apache 2.0 Licensed */\n"
-      },
-      release: {
-        files: {
-          'release/howerest-sdkzer.js': ['release/howerest-sdkzer.js']
-        }
-      }
     },
 
     //  Karma runner (BDD)
@@ -58,8 +19,6 @@ module.exports = function(grunt) {
 
   // grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-karma');
 
   grunt.registerTask('default', [/*'ts', */ 'concat', 'uglify']);
