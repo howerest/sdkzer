746a1f1e06e9f6e20c23fc40c943118a34e080bf
DavidValin <skkunk@gmail.com>
2017-01-10 19:12:04 +0100

    ...

---
+++ built/spec/sdkzer_spec.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="../../node_modules/@types/jasmine/index.d.ts" />
+/// <reference path="../../node_modules/@types/jasmine-ajax/index.d.ts" />
+/// <reference path="../../node_modules/@types/es6-promise/index.d.ts" />
--- built/src/spec/sdkzer_spec.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="../../../node_modules/@types/jasmine/index.d.ts" />
-/// <reference path="../../../node_modules/@types/jasmine-ajax/index.d.ts" />
-/// <reference path="../../../node_modules/@types/es6-promise/index.d.ts" />
-/// <reference path="../../node_modules/js-webservices/ts/web_services.d.ts" />
