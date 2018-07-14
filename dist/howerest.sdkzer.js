27e486193e552fcf1f9edcf150f39fd1798d490a
David Valin <hola@davidvalin.com>
2018-07-15 00:54:47 +0100

    Modernize dev tools. More reporting and docs
    - Migrate build from Grunt to webpack
    - Migrated from karma to jest
    - Move tests under test
    - Added coverage report
    - Stop using "typings". Use @types definitions
    - From npm to yarn
    - Adapt travis pipeline to use new setup
    - Moved to UMD instead of CommonJS to support Node and Browser
    - Updated README: added SDK demo link and other instructions

--- dist/howerest.sdkzer.js
+++ dist/howerest.sdkzer.js
@@ -1,6 +1,109 @@
+/*! sdkzer 1.6.0 - By David Valin - www.davidvalin.com */
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else {
+		var a = factory();
+		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
+	}
+})(this, function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
 /* --------------------------------------------------------------------------
 
-    howerest 2016 - <davidvalin@howerest.com> | www.howerest.com
+    howerest 2018 - <hola@davidvalin.com> | www.howerest.com
 
     Apache 2.0 Licensed
     -------------------
@@ -12,10 +115,9 @@
     2. Start consuming your resource
 
 --------------------------------------------------------------------------- */
-"use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var js_webservices_1 = require("js-webservices");
-var Sdkzer = (function () {
+var js_webservices_1 = __webpack_require__(1);
+var Sdkzer = /** @class */ (function () {
     /**
      * Creates an instance of a model entity with an API to communicate with
      * a resource (http RESTful resource)
@@ -450,34 +552,46 @@ var Sdkzer = (function () {
         });
         return instancePromise;
     };
+    // Configuration
+    Sdkzer.DEFAULT_HTTP_HEADERS = [];
+    Sdkzer.HTTP_PATTERN = 'restful_crud';
+    Sdkzer.PARENTS_FETCH_STRATEGY = 'none';
+    Sdkzer.HTTP_QUERY_GUESS_CONFIG = {
+        "restful_crud": {
+            "read_collection": {
+                verb: "GET",
+                endpoint: ''
+            },
+            "read_record": {
+                verb: "GET",
+                endpoint: ''
+            },
+            "create_record": {
+                verb: "GET",
+                endpoint: ''
+            },
+            "update_record": {
+                verb: "GET",
+                endpoint: ''
+            },
+            "delete_record": {
+                verb: "GET",
+                endpoint: ''
+            }
+        }
+    };
     return Sdkzer;
 }());
-// Configuration
-Sdkzer.DEFAULT_HTTP_HEADERS = [];
-Sdkzer.HTTP_PATTERN = 'restful_crud';
-Sdkzer.PARENTS_FETCH_STRATEGY = 'none';
-Sdkzer.HTTP_QUERY_GUESS_CONFIG = {
-    "restful_crud": {
-        "read_collection": {
-            verb: "GET",
-            endpoint: ''
-        },
-        "read_record": {
-            verb: "GET",
-            endpoint: ''
-        },
-        "create_record": {
-            verb: "GET",
-            endpoint: ''
-        },
-        "update_record": {
-            verb: "GET",
-            endpoint: ''
-        },
-        "delete_record": {
-            verb: "GET",
-            endpoint: ''
-        }
-    }
-};
 exports.Sdkzer = Sdkzer;
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+module.exports = require("js-webservices");
+
+/***/ })
+/******/ ]);
+});
+//# sourceMappingURL=howerest.sdkzer.js.map
