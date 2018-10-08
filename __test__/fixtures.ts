4eb1ec0939d15058ce4550d63f1f71fc4a63eb5c
David Valin <hola@davidvalin.com>
2018-10-08 22:54:33 +0100

    Split parse. ts-config, outDir
    1. Splitted $parse into:
    - parseRecord: to parse a single record
    - parseCollection: to parse a collection of records
    
    2. Test only Sdkzer default functionality, not derivated parses
    
    3. Some issues fixed:
    - added ts-lader as devDependency, was picking it up from global installed packages
    - added "outDir" to specify the typings generation path when running webpack build

--- __test__/fixtures.ts
+++ __test__/fixtures.ts
@@ -16,36 +16,12 @@ export class Item extends Sdkzer {
     return 'http://api.mydomain.com/v1/items';
   }
 
+
   public defaults() {
     return {
       name: "A good choice",
       items: [1, 100, 1, 60]
     };
-  }
-
-  public parseOne(data) {
-    // Parse record
-    var json = {
-      id: data.id,
-      name: data.name,
-      items: data.items
-    };
-
-    return json;
-  }
-
-  public parse(data) {
-    var parsed = [];
-
-    if (Array.isArray(data)) {
-      for (var i = 0; i < data.length; i++) {
-        parsed.push(this.parseOne(data[i]));
-      }
-    } else {
-      parsed.push(this.parseOne(data.folder));
-    }
-
-    return parsed;
   }
 }
 
