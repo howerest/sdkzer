3466c6fd761c37d79a5e70ef398812f1ebd94996
David Valin <hola@davidvalin.com>
2019-01-13 23:35:44 +0000

    0.7.2 - Upgrade js-webservices to fix cookies not being sent

--- webpack.conf.js
+++ webpack.conf.js
@@ -39,7 +39,7 @@ module.exports = {
   plugins: [
       // new CheckerPlugin(),
       new webpack.BannerPlugin({
-        banner: "sdkzer 0.7.1 - By David Valin - www.davidvalin.com",
+        banner: "sdkzer 0.7.2 - By David Valin - www.davidvalin.com",
         entryOnly: true
     })
   ]
