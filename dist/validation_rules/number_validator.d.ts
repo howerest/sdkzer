252ccc62b0ab0c0cf9c9070363a863a1eaf0f6eb
David Valin <hola@davidvalin.com>
2019-05-06 01:15:35 +0100

    0.7.3 release

--- dist/validation_rules/number_validator.d.ts
+++ dist/validation_rules/number_validator.d.ts
@@ -1,4 +1,4 @@
 import { ValidationRule } from '../validation_rule';
-export declare class MinMaxNumberValidator extends ValidationRule {
+export declare class NumberValidator extends ValidationRule {
     protected conditions: Array<Function>;
 }
